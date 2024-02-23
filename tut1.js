f106d9bc660266ea71d932a11db1750161cf80cd
Gautam <gauta.m.endiratta@gmial.com>
2024-02-23 15:18:48 +0530

    Initial Commit

---
+++ tut1.js
@@ -0,0 +1,62 @@
+console.log("Hello world");
+
+// var string = "Hello world";
+// var number = 15;
+// // var c = 12.32;
+// var boolean = true;
+// var d = null;
+// var e = undefined;
+
+
+let string = "Hello world";
+let number = 15;
+// var c = 12.32;
+let boolean = true;
+let d = null;
+let e = undefined;
+
+let object = {
+name: "Gautam",
+age: 23,
+"roll no": 123456,
+}
+
+object.phone = 9650324809;
+object.marks = 50;
+
+
+
+console.log(string, number, boolean, d, e);
+console.log(typeof string, typeof number, typeof boolean, typeof d, typeof e);
+
+console.log(object);
+
+// PRACTISE SET
+
+let string_ = "Gautam";
+let number_ = 2;
+
+let answer = string_+ number_;
+
+console.log(answer);
+console.log(typeof answer);
+
+const obj ={
+  name: "gautam",
+  age: 23
+}
+
+obj.place = "Faridabad";
+
+console.log(obj);
+
+let dictionary = {
+  word1: "This is word1",
+  word2: "This is word2",
+  word3: "This is word3",
+  word4: "This is word4",
+  word5: "This is word5",
+}
+
+console.log(dictionary);
+
