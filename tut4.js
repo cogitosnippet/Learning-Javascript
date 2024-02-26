638e714ac2e6faeda9f9a87db36f6fc41017f4cf
Gautam <gauta.m.endiratta@gmial.com>
2024-02-26 23:05:38 +0530

    Adding New File

---
+++ tut4.js
@@ -0,0 +1,27 @@
+console.log("tut4");
+
+console.log("har\"".length);
+
+let real_name = "gautam";
+
+console.log(real_name.startsWith("ga"));
+
+console.log(real_name.includes("auta"));
+
+
+
+let ends = real_name.endsWith("tam");
+console.log(ends);
+
+
+let lower = "GAUTAM";
+
+console.log(lower.toLowerCase());
+
+let extraction = "Please give Rs 1000";
+
+console.log(extraction.slice(15));
+
+extraction[3]="g";
+
+console.log(extraction);
